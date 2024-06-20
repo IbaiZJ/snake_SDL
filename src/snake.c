c07e46645a62f70c1094097b3d51dbda39d80931
Ibai <IIbaIIZJ@gmail.com>
2024-06-20 14:16:24 +0200

    remove all

--- src/snake.c
+++ src/snake.c
@@ -1,14 +1,55 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "SDL.h"
 
-int main(int argc, char** argv) {
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 800
+#define WINDOW_NAME "SNAKE"
+
+SDL_Window* window;
+SDL_Renderer* renderer;
+SDL_Event event;
+
+int createWindowAndRender() {
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         return 1;
     }
+
+
+    window = SDL_CreateWindow(
+        WINDOW_NAME, 
+        SDL_WINDOWPOS_UNDEFINED, 
+        SDL_WINDOWPOS_UNDEFINED, 
+        WINDOW_WIDTH, WINDOW_HEIGHT, 
+        SDL_WINDOW_SHOWN
+    );
     
+    if (window == NULL) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        // SDL_Quit();
+        return 1;
+    }
     
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if(!renderer) {
+        fprintf(stderr, "unable to set renderer!\n");
+    }
+
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    if(createWindowAndRender()) return 1;
+
     
+
+
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
     
     return 0;
 }
